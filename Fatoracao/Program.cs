c88d8d1ad5aaf7d83d28b587d9c2deef8ecf2f23
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-03 14:05:25 -0300

    Add project files.

---
+++ Fatoracao/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numero = 0;
+        double controle = 0;
+        double produto = 1;
+
+        Console.WriteLine("Informe o numero:");
+        numero = double.Parse(Console.ReadLine());
+        controle = numero;
+
+        if (numero == 0)
+        {
+            Console.WriteLine("1");
+        }
+
+        if (numero != 0  && numero > 0)
+        {
+            for(int i = 0; i < controle; i++)
+            {
+                produto *= numero;
+                numero--;
+
+            }
+            Console.WriteLine("Resultado:");
+            Console.WriteLine(produto);
+        }
+
+    }
+}
